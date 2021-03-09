2c803e810be8079fad6740e37ce1b352ae8712c4
Bel0g0r <46893238+Bel0g0r@users.noreply.github.com>
2021-03-09 18:31:04 +0300

    Суши Тайм -Воронеж - 1 версия

---
+++ precache-manifest.314fd075b8736753bf85944213e227a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54899623960117808f21e22dfac7b6bb",
+    "url": "/sushi-time-vrn.github.io/index.html"
+  },
+  {
+    "revision": "c354d6fc617571ef1d2c",
+    "url": "/sushi-time-vrn.github.io/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "1c027d68d41254a3a656",
+    "url": "/sushi-time-vrn.github.io/static/css/main.884ceb27.chunk.css"
+  },
+  {
+    "revision": "c354d6fc617571ef1d2c",
+    "url": "/sushi-time-vrn.github.io/static/js/2.4be08a3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sushi-time-vrn.github.io/static/js/2.4be08a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c027d68d41254a3a656",
+    "url": "/sushi-time-vrn.github.io/static/js/main.ba38d168.chunk.js"
+  },
+  {
+    "revision": "2507ca82bf233acd1aa9",
+    "url": "/sushi-time-vrn.github.io/static/js/runtime-main.92b463dc.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/sushi-time-vrn.github.io/static/media/persik.4e1ec840.png"
+  }
+]);
